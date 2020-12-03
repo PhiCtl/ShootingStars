8b910076ad4973cb33aae82d35d7695369432b89
Stella Petronio <stella.petronio@epfl.ch>
2020-12-03 11:48:32 +0100

    Update Jacobi

--- Linear_solvers/src/Jacobi.cpp
+++ Linear_solvers/src/Jacobi.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "Jacobi.h"
+template <typename T>
+Jacobi<T>::Jacobi(const Vector<T>& vec, int nb_iters, double tol) : Iterative_Solver<T>(vec, nb_iters, tol){};
+
+template <typename T>
+Jacobi<T>::Jacobi() : Iterative_Solver<T>(){};
+
+template <typename T>
+Jacobi<T>::~Jacobi(){};
+
+template <typename T>
+Jacobi<T>::Jacobi(const Jacobi<T>& solver){
+    this-> initial_guess = solver.initial_guess;
+    this-> nb_iter = solver.nb_iter;
+    this-> tol = solver.tol;
+}
+
+template <typename T>
+Vector<T> Jacobi<T>::Solve(const Matrix<T>& A, const Vector<T>& b) {
+    try {
+        if (this->initial_guess.getRows() <=1)
+            this->initial_guess = Vector<T>(A.getCols());
+
+        int n = A.getCols(); //A must be a square matrix
+        Vector<T> x = this->initial_guess;
+        Vector<T> r = A*x - b;
+        T res = r.Norm();
+        size_t iter(0);
+
+        while((res > this->tol) && (iter < this->nb_iter)){
+            for(int i = 0; i < n; i++){
+                auto sum = 0; //is it correct auto?
+                for(int j = 0; j < n; j++){
+                    if(j != i){
+                        sum += A(i,j) + x(j);
+                    }
+                }
+                x[i] = 1/A(i,i) * (b(i) - sum);
+            }
+            iter += 1;
+        }
+
+    }catch(const runtime_error& e) {
+    cout << e.what() << endl;
+ }
+}
+
+
--- Linear_solvers/src/Jacobi.h
+++ Linear_solvers/src/Jacobi.h
@@ -5,9 +5,27 @@
 #ifndef LINEAR_SOLVERS_JACOBI_H
 #define LINEAR_SOLVERS_JACOBI_H
 
-#include "NonIterative_Solver.h"
+#include "Iterative_Solver.h"
 template <typename T>
-class Jacobi : public NonIterative_Solver<T>{
+class Jacobi : public Iterative_Solver<T>{
+public:
+    /*
+     * Constructors:
+     * three paramenters: initial guess vector, number of iterations, tolerance
+     * default
+     * copy constructor
+     *
+    */
+
+    Jacobi(const Vector<T>&, int, double);
+    Jacobi();
+    Jacobi(const Jacobi<T>&);
+
+    //Destructor
+    ~Jacobi() override;
+
+    //Linear algebra
+    Vector<T> Solve(const Matrix<T>& A, const Vector<T>& b) override;
 
 
 };
