8fdb8b70c921f90669bc97aef4cf6450a778722b
Des Courtils Philippine Marie Joseph <descourt@INTRANET.EPFL.CH@MA-LINUX-06.intranet.epfl.ch>
2020-12-11 09:41:41 +0100

    Vector test ok

--- Linear_solvers/test/VectorTest.cpp
+++ Linear_solvers/test/VectorTest.cpp
@@ -12,12 +12,12 @@ TEST(VectorBuilderTest, vector_constructors)
 
     Vector<double> vec2(9);
     EXPECT_EQ(vec2.getRows(),9);
-    EXPECT_EQ(vec1.getCols(),1);
-    EXPECT_EQ(vec1.getValue(), vector<double>(9));
+    EXPECT_EQ(vec2.getCols(),1);
+    EXPECT_EQ(vec2.getValue(), vector<double>(9));
 
     Vector<double> vec3(6,1);
-    EXPECT_EQ(vec2.getRows(),6);
-    EXPECT_EQ(vec1.getCols(),1);
+    EXPECT_EQ(vec3.getRows(),6);
+    EXPECT_EQ(vec3.getCols(),1);
 }
 
 TEST(VectorBuilderTest, vector_elements)
@@ -33,7 +33,14 @@ TEST(VectorBuilderTest, vector_elements)
     Vector<double> vec3(val2);
     EXPECT_EQ(vec2.getValue(), val2);
 
-    EXPECT_EQ(vec3[0], 1);
+    EXPECT_EQ(vec3(0), 1);
+
+    vec3[0] = 22;
+    EXPECT_EQ(vec3(0),22);
+
+    vec3.Push_back(56);
+    EXPECT_EQ(vec3.getRows(),4);
+    EXPECT_EQ(vec3(3),56);
 }
 
 TEST(VectorBuilderTest, copy_constructors)
@@ -44,7 +51,41 @@ TEST(VectorBuilderTest, copy_constructors)
 
     Vector<double> vec2;
     vec2 = vec1;
-    EXPECT_EQ(vec2.)
+    EXPECT_EQ(vec2.getValue(),vec1.getValue());
+
+    Matrix<double> mat(3,1,2);
+    vec2 =  mat;
+    EXPECT_EQ(vec2.getValue(), vector<double>(3,2));
+
+}
+
+TEST(VectorBuilder, not_allowed)
+{
+    Matrix<double> M(4,5,1);
+    ASSERT_THROW(Vector<double> vec1(M),runtime_error);
+}
+
+TEST(VectorOperations, allowed_operations)
+{
+    Vector<double> vec1(1,2);
+    Vector<double> vec2(1,4);
+    EXPECT_EQ(vec2/vec1, 2);
+
+    Vector<double> vec3(3,1);
+    Vector<double> vec4(3,2);
+    EXPECT_EQ(vec3*vec4, 6);
+    vec4 = vec3*3;
+    EXPECT_EQ(vec4(0),3);
+}
+
+TEST(VectorOperations, not_allowed)
+{
+    Vector<double> vec1(3,1);
+    Vector<double> vec2(3,2);
+    ASSERT_THROW(vec1/vec2, runtime_error);
+
+    vec2 = vector<double>(4,1);
+    ASSERT_THROW(vec1*vec2, runtime_error);
 
 }
 
