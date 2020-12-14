03b21e06a284fddd5a9c07588bc8b0358abb2ec4
Stella Petronio <stella.petronio@epfl.ch>
2020-12-14 10:01:30 +0100

    solver test

--- Linear_solvers/test/ItSolverTest.cpp
+++ Linear_solvers/test/ItSolverTest.cpp
@@ -5,6 +5,7 @@
 #include <Conjugate_Gradient.h>
 #include <Gauss_Seidel.h>
 #include <Richardson.h>
+#include "FileReader.h"
 
 template<int test>
 class IterativeTestReal:public::testing::Test{
@@ -22,75 +23,71 @@ public:
                 b = {7,8,8,7};
                 sol = {1,1,1,1};
                 break;
+            case 2:
+                f = FileReader("MatSDP.mat", "Vec.mat", false)
+                f.setFiles("MatSDP.mat", "Vec.mat")
+                break;
             default:
                 break;
         }
     }
 
-    //Matrix<double> A;
+    Matrix<double> A_file;
     vector<vector<double>> A;
     vector<double> b;
-    //Vector<double> b;
+    Vector<double> b_file;
     vector<double> sol;
+    FileReader f;
 };
 
-using IterativeTestRealF1 = IterativeTestReal<0>;
-using IterativeTestRealF2 = IterativeTestReal<1>;
+using IterativeTestRealF0 = IterativeTestReal<0>;
+using IterativeTestRealF1 = IterativeTestReal<1>;
+using IterativeTestRealF2 = IterativeTestReal<2>;
 
-TEST_F(IterativeTestRealF1, Conjugate_Gradient){
-
+TEST_F(IterativeTestRealF0, Conjugate_Gradient){
     Conjugate_Gradient<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
-TEST_F(IterativeTestRealF1, Jacobi){
-
+TEST_F(IterativeTestRealF0, Jacobi){
     Jacobi<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
 
-TEST_F(IterativeTestRealF1, Gauss_Seidel){
-
+TEST_F(IterativeTestRealF0, Gauss_Seidel){
     Gauss_Seidel<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
-TEST_F(IterativeTestRealF1, Richardson){
-
+TEST_F(IterativeTestRealF0, Richardson){
     Richardson<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
-TEST_F(IterativeTestRealF2, Conjugate_Gradient1){
-
+TEST_F(IterativeTestRealF1, Conjugate_Gradient1){
     Conjugate_Gradient<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
-TEST_F(IterativeTestRealF2, Jacobi1){
-
+TEST_F(IterativeTestRealF1, Jacobi1){
     Jacobi<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
 
-TEST_F(IterativeTestRealF2, Gauss_Seidel1){
-
+TEST_F(IterativeTestRealF1, Gauss_Seidel1){
     Gauss_Seidel<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
-
 }
 
-TEST_F(IterativeTestRealF2, Richardson1){
-
+TEST_F(IterativeTestRealF1, Richardson1){
     Richardson<double> solver;
     ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
+}
 
+TEST_F(IterativeTestRealF2, Conjugate_Gradient2){
+    Matrix
+    Conjugate_Gradient<double> solver;
+    ASSERT_NEAR(solver.Solve(A,b).getValue()[0], sol[0], 1e-4);
 }
