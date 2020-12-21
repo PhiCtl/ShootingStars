e09d68ed1ea8f3ca049dc2bdeff6162f58853ed5
Des Courtils Philippine Marie Joseph <descourt@INTRANET.EPFL.CH@MA-LINUX-02.intranet.epfl.ch>
2020-12-21 16:44:14 +0100

    solved circular dependencies

--- Linear_solvers/src/LinearSolver.h
+++ Linear_solvers/src/LinearSolver.h
@@ -5,8 +5,6 @@
 #ifndef LINEAR_SOLVERS_SOLVER_H
 #define LINEAR_SOLVERS_SOLVER_H
 
-#endif //LINEAR_SOLVERS_SOLVER_H
-
 #include "Vector.h"
 
 template <typename T> class LinearSolver{
@@ -15,3 +13,5 @@ public:
     virtual ~LinearSolver() {};
     virtual Vector<T> Solve(const Matrix<T>&, const Vector<T>&) {};
 };
+
+#endif //LINEAR_SOLVERS_SOLVER_H
