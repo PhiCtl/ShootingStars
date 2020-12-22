2563dbef580f95e3cba608c05d5be0fa3da4e084
Stella Petronio <stella.petronio@epfl.ch>
2020-12-22 22:31:53 +0100

    doxygen for Jacobi

--- Linear_solvers/src/Jacobi.h
+++ Linear_solvers/src/Jacobi.h
@@ -6,7 +6,8 @@
 #define LINEAR_SOLVERS_JACOBI_H
 
 #include "Iterative_Solver.h"
-
+/*! @brief Jacobi solver
+ */
 template <typename T>
 class Jacobi : public Iterative_Solver<T>{
 public:
@@ -17,15 +18,33 @@ public:
      * copy constructor
      *
     */
-
+    /*! @brief Constructor
+     * @param init: initial conditions
+     * @param iter: number of iterations
+     * @param threshold: stopping criteria (if residual error is below this threshold)
+     */
     Jacobi(const Vector<T>&, int, double);
+
+    /*! @brief Constructor (default)
+     */
     Jacobi();
+
+    /*! @brief Copy constructor
+     * @param solver: another Jacobi solver of same type
+     */
     Jacobi(const Jacobi<T>&);
 
     //Destructor
+    /*! @brief Destructor (default)
+     */
     ~Jacobi() override;
 
     //Linear algebra
+    /*! @brief Solve
+     * @param A: Matrix of same type T as solver
+     * @param b: Vector of same type T as solver
+     * @return x: a Vector x such that Ax = b
+     */
     Vector<T> Solve(const Matrix<T>& A, const Vector<T>& b) override;
 
 
