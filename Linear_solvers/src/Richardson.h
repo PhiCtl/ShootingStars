3b802001ac21c90b420d1e33222e8562e8e4b95d
Stella Petronio <stella.petronio@epfl.ch>
2020-12-22 22:34:44 +0100

    doxygen for Richardson

--- Linear_solvers/src/Richardson.h
+++ Linear_solvers/src/Richardson.h
@@ -6,7 +6,8 @@
 #define LINEAR_SOLVERS_RICHARDSON_H
 
 #include "Iterative_Solver.h"
-
+/*! @brief Richardson solver
+ */
 template <typename T>
 class Richardson : public Iterative_Solver<T>{
 public:
@@ -18,14 +19,33 @@ public:
      *
     */
 
+    /*! @brief Constructor
+     * @param init: initial conditions
+     * @param iter: number of iterations
+     * @param threshold: stopping criteria (if residual error is below this threshold)
+     */
     Richardson(const Vector<T>&, int, double);
+
+    /*! @brief Constructor (default)
+     */
     Richardson();
+
+    /*! @brief Copy constructor
+     * @param solver: another Richardson solver of same type
+     */
     Richardson(const Richardson<T>&);
 
     //Destructor
+    /*! @brief Destructor (default)
+     */
     ~Richardson() override;
 
     //Linear algebra
+    /*! @brief Solve
+     * @param A: Matrix of same type T as solver
+     * @param b: Vector of same type T as solver
+     * @return x: a Vector x such that Ax = b
+     */
     Vector<T> Solve(const Matrix<T>& A, const Vector<T>& b) override;
 
 
