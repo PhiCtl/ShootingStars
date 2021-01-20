5fd38ce80bde5838526306be08c6e4dc1cf379d1
Des Courtils Philippine Marie Joseph <descourt@INTRANET.EPFL.CH@MA-LINUX-10.intranet.epfl.ch>
2021-01-20 09:45:31 +0100

    added scalar conversion tests

--- Linear_solvers/test/VectorTest.cpp
+++ Linear_solvers/test/VectorTest.cpp
@@ -128,7 +128,7 @@ TEST(VectorOperations, multiplication_allowed)
 {
     Vector<double> vec1(3,1);
     Vector<double> vec2(3,2);
-    EXPECT_EQ(vec1*vec2, 6);
+    EXPECT_EQ(vec1.transpose()*vec2, 6);
     vec1 = vec2*3;
     EXPECT_EQ(vec1(0),6);
 }
@@ -140,7 +140,7 @@ TEST(VectorOperations, not_allowed)
     ASSERT_THROW(vec1/vec2, runtime_error);
 
     vec2 = vector<double>(4,1);
-    ASSERT_THROW(vec1*vec2, runtime_error);
+    ASSERT_THROW(vec1.transpose()*vec2, invalid_argument);
 
 }
 
@@ -152,6 +152,13 @@ TEST_F(Operation1, complex_operations_1)
 TEST_F(Operation2, complex_operations_2)
 {
     Compare();
+}
+
+TEST(Operation3, scalar_conversion)
+{
+    Vector<double> Vec1(5,3);
+    Vector<double> Vec2(5,2);
+    EXPECT_DOUBLE_EQ(Vec2.transpose()*Vec1, 30);
 }
 
 int main(int argc, char** argv)
